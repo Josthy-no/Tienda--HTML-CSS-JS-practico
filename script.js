8d5c51034626eed54a32a5489e82f805db723ebc
Whitehost <camalle2006@gmail.com>
2023-12-05 15:59:29 -0500

    se agrego la listra de productos ilimitada desde js

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const iconMenuMobile = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
 const productDetail = document.querySelector('.product-detail')
 const iconProducts = document.querySelector('.navbar-shopping-cart')
+const cardsContainer = document.querySelector('.cards-container')
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
 function toggleDesktopMenu(params) {
     desktopMenu.classList.toggle('inactive'); 
@@ -21,4 +23,54 @@ function toggleDesktopProducts(params) {
     mobileMenu.classList.add('inactive');
     desktopMenu.classList.add('inactive'); 
     productDetail.classList.toggle('inactive');
+}
+
+// lista de productos
+let productList =[]
+productList.push( {
+    name:'bike',
+    price:129,
+    image :'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+productList.push( {
+    name:'skate',
+    price:100,
+    image :'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+productList.push( {
+    name:'motorbike',
+    price:129,
+    image :'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+
+for(product of productList){
+
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.image);
+
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info')
+
+    const productInfoDiv = document.createElement('div');
+
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+    const productName = document.createElement('p');
+    productName.innerText = product.productName;
+
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img')
+    productImgCart.setAttribute('src' , './icons/bt_add_to_cart.svg')
+
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure)
+    productInfoFigure.appendChild(productImgCart)
+    productCard.appendChild(productImg)
+    productCard.appendChild(productInfo)
+    cardsContainer.appendChild(productCard)
 }
