b71dde9ad9caec7310a0e93baba530661affbe2e
Whitehost <camalle2006@gmail.com>
2023-12-06 19:10:07 -0500

    se agrego un aside de la informacion del elemento seleccionado(estandar) falta automatizar

--- script.js
+++ script.js
@@ -2,20 +2,28 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu')
 const iconMenuMobile = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
+
 const productDetail = document.querySelector('.product-detail')
 const iconProducts = document.querySelector('.navbar-shopping-cart')
+
 const cardsContainer = document.querySelector('.cards-container')
+
+const elementInfoContainer = document.querySelector('.product-detail-selected')
+const iconCloseElementInfo = document.querySelector('.product-detail-close-selected')
+iconCloseElementInfo.addEventListener('click', elementInfoClose)
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 function toggleDesktopMenu(params) {
     desktopMenu.classList.toggle('inactive'); 
     productDetail.classList.add('inactive');
+    elementInfoClose ()
 }
 
 iconMenuMobile.addEventListener('click', toggleMobileMenu)
 function toggleMobileMenu(params) {
       mobileMenu.classList.toggle('inactive');
-        productDetail.classList.add('inactive'); 
+        productDetail.classList.add('inactive');
+        elementInfoClose () 
 }
 
 iconProducts.addEventListener('click' , toggleDesktopProducts)
@@ -23,6 +31,7 @@ function toggleDesktopProducts(params) {
     mobileMenu.classList.add('inactive');
     desktopMenu.classList.add('inactive'); 
     productDetail.classList.toggle('inactive');
+    elementInfoClose ()
 }
 
 // lista de productos
@@ -43,6 +52,11 @@ productList.push( {
     image :'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 })
 
+// las clases que contengan la palabra product se refieren a la lista de compra
+
+// las clases que contengan la palabra element se refieren a la informacion del producto
+
+
 function renderProducts (array){
     for(product of array){
 
@@ -51,6 +65,7 @@ function renderProducts (array){
     
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.image);
+        productImg.addEventListener('click' , elementInfoOpen)
     
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info')
@@ -60,7 +75,7 @@ function renderProducts (array){
         const productPrice = document.createElement('p');
         productPrice.innerText = '$' + product.price;
         const productName = document.createElement('p');
-        productName.innerText = product.productName;
+        productName.innerText = product.name;
     
         const productInfoFigure = document.createElement('figure');
         const productImgCart = document.createElement('img')
@@ -77,3 +92,14 @@ function renderProducts (array){
     }
 }
 renderProducts(productList)
+
+// informacion del producto / elemento  para abrir o cerrar
+function elementInfoOpen () {
+    elementInfoContainer.classList.remove('inactive')
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive'); 
+    productDetail.classList.add('inactive');
+}
+function elementInfoClose () {
+    elementInfoContainer.classList.add('inactive')
+}
